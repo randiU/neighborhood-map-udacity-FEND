6b1f70e0f5b3d3da7ebdfec67dfde08e35bef336
randiU <33576756+randiU@users.noreply.github.com>
2019-01-04 14:59:20 -0700

    info windows now close automatically when another is clicked

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-   
+    //Grabbing venue data and updating the venues state to returned array
     neighborhoodAPI.getVenues().then(data => {
       console.log(data.response.venues);
       this.setState({
@@ -26,36 +26,66 @@ class App extends Component {
     })
   }
 
+
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDd7-K4U0STJIqxo--RD9D_XHTuUx1VJ8s&callback=initMap")
     window.initMap = this.initMap
   }
 
+//Creating the google map
   initMap = () => {
    const map = new window.google.maps.Map(document.getElementById('map'), {
       center: this.state.center,
       zoom: this.state.zoom
     });
 
+   const largeInfowindow = new window.google.maps.InfoWindow();
+   //mapping over the venue array to create markers and related info windows
    this.state.venues.map(myVenue => {
 
+    let contentString = `${myVenue.name}`
+    //info window
+    const infowindow = new window.google.maps.InfoWindow({
+          content: contentString
+        });
     //Google Maps Platform, Markers
     const marker = new window.google.maps.Marker({
       position: {lat: myVenue.location.lat, lng: myVenue.location.lng},
       map: map,
       title: myVenue.name
-
     });
 
+    marker.addListener('click', function() {
+          populateInfoWindow(this, largeInfowindow);
+        });
+
     console.log(neighborhoodAPI.getVenueDetails(myVenue.id));
-   })
-   
-   
+   })   
+
+   // Udacity Getting Started With APIs course
+   // This function populates the infowindow when the marker is clicked. We'll only allow
+   // one infowindow which will open at the marker that is clicked, and populate based
+   // on that markers position.
+  function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick',function(){
+        infowindow.setMarker = null;
+      });
+    }  
+    }
   }
 
   render() {
     return (
       <main>
+        <div className= "header">
+          <h1> Downtown Boise Loves Coffee & Donuts </h1>
+        </div>
         <div className= "container">
           <div id='map'></div>
         
